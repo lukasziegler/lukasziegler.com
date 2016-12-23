9276b36658e6930b72684b707112213106753e03
lukasziegler <lukas@lukasziegler.com>
2016-12-23 13:30:14 +0100

    gulp build & live-reload
    adds live-reload of files via browserSync
    auto-compiles stylus files & sourcemap generation
    
    minor updates to styles & about me text

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,41 @@
-var os = require('os');
 var gulp = require('gulp');
-var open = require('gulp-open');
+var stylus = require('gulp-stylus');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 
-gulp.task('default', function () {
-  gulp.start('watch');
+var path = {
+  html: '*.html',
+  js: 'js/**/*.js',
+  styles: 'css/**/*.styl',
+  outputCss: 'css',
+  img: 'img'
+};
+
+gulp.task('styles', function () {
+  gulp.src(path.styles)
+    .pipe(sourcemaps.init())
+    .pipe(stylus({
+      paths: ['node_modules'],
+      import: ['rupture/rupture'],
+      'include css': true,
+      compress: true
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(path.outputCss))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function () {
-  gulp.src('./index.html')
-    .pipe(open());
+gulp.task('observeFiles', function () {
+  gulp.watch(path.styles, ['styles']);
+  gulp.watch(path.html).on('change', reload);
 });
+
+gulp.task('browserSync', function () {
+  browserSync.init({
+    server: true
+  });
+});
+
+gulp.task('default', ['styles', 'observeFiles', 'browserSync']);
+gulp.task('watch', ['styles', 'observeFiles']);
