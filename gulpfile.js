721377c37746781486f506d1c372f7775f8b4b09
lukasziegler <lukas@lukasziegler.com>
2016-12-25 12:20:00 +0100

    extract production code & minify html on build
    separate the production code from the stylus / gulp source code, now all
    production code resides in the `/www/` subfolder
    
    adds `gulp-htmlmin` for html minification
    
    DEPRECATED: /www/css/ subfolder - all styles there will become obsolete
    one by one and if needed the logic will be integrated into the minified
    css bundle

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,19 @@
-'use strict'
+/*jslint node: true */
+'use strict';
 
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 var sourcemaps = require('gulp-sourcemaps');
+var htmlmin = require('gulp-htmlmin');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 
 var path = {
   html: '*.html',
   js: 'js/**/*.js',
-  styles: 'css/**/*.styl',
-  outputCss: 'css',
-  img: 'img'
+  styles: 'styles/**/*.styl',
+  output: 'www',
+  img: 'www/img'
 };
 
 gulp.task('styles', function () {
@@ -19,25 +21,36 @@ gulp.task('styles', function () {
     .pipe(sourcemaps.init())
     .pipe(stylus({
       paths: ['node_modules'],
-      import: ['rupture/rupture'],
       'include css': true,
       compress: true
     }))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(path.outputCss))
+    .pipe(gulp.dest(path.output))
     .pipe(browserSync.stream());
+});
+
+gulp.task('minify', function () {
+  return gulp.src(path.html)
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest(path.output));
 });
 
 gulp.task('observeFiles', function () {
   gulp.watch(path.styles, ['styles']);
+  gulp.watch(path.html, ['minify']);
   gulp.watch(path.html).on('change', reload);
 });
 
 gulp.task('browserSync', function () {
   browserSync.init({
-    server: true
+    server: {
+      baseDir: 'www',
+      index: 'index.html'
+    }
   });
 });
 
-gulp.task('default', ['styles', 'observeFiles', 'browserSync']);
-gulp.task('watch', ['styles', 'observeFiles']);
+gulp.task('build', ['styles', 'minify']);
+gulp.task('default', ['build', 'observeFiles', 'browserSync']);
